5cd94960cdeccd9aaa58f166cf513b288a1ce455
Stanley Enow Lekunze <hguruman@gmail.com>
2024-06-19 00:27:44 +0400

    Update code to add API integration

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\API\V1\ArticleController;
+use App\Http\Controllers\API\V1\AuthorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function(){
+    Route::apiResource('/artcles', ArticleController::class);
+
+    Route::get('/authors/{user}', [AuthorController::class, 'show'])->name('authors');
+});
